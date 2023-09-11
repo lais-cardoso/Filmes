0847cd9871231cbc344f83b35f104c26511f649a
lais-cardoso <lalaicardoso27@hotmail.com>
2023-09-11 11:43:07 -0300

    feat: add type to function variables

--- lib/dates.py
+++ lib/dates.py
@@ -1,6 +1,6 @@
-from datetime import timedelta
+from datetime import date, timedelta
 
-def calculate_expired_date(current_date):
+def calculate_expired_date(current_date: date):
     # index route calculation
     free_sample = timedelta(30)
     unsubscribe_date = current_date + free_sample
@@ -8,7 +8,7 @@ def calculate_expired_date(current_date):
 
     return expired_date
 
-def calculate_difference_day(current_date, oscar_date):
+def calculate_difference_day(current_date: date, oscar_date: date):
     # home route calculation
     difference = oscar_date - current_date
     difference_day = difference.days
