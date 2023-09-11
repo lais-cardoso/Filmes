9d13aa86d002d698b59262d051c6c6e84e65e90a
lais-cardoso <lalaicardoso27@hotmail.com>
2023-09-11 08:21:13 -0300

    style: change folder dates file

---
+++ lib/dates.py
@@ -0,0 +1,20 @@
+#arquivo para colocar as funções
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+def calculate_expired_date(current_date):
+    # index route calculation
+    free_sample = timedelta(30)
+    unsubscribe_date = current_date + free_sample
+    expired_date = unsubscribe_date.strftime('%d/%m/%Y')
+
+    return expired_date
+
+def calculate_difference_day(current_date, oscar_date):
+    # home route calculation
+    difference = oscar_date - current_date
+    difference_day = difference.days
+
+    return difference_day
