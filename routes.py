6eab1668712f97821bb8e3b885ecf59c2d821d33
lais-cardoso <lalaicardoso27@hotmail.com>
2023-09-12 08:47:36 -0300

    merge

--- routes.py
+++ routes.py
@@ -13,3 +13,32 @@ def login():
         register_form(name, email, age)
     return render_template('register.html')
 
+from flask import Flask, render_template
+from datetime import datetime
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+#local
+import lib.dates as dates
+
+app = Flask(__name__)
+
+@app.route('/about')
+def index():
+    current_date = datetime.now()
+    expired_date = dates.calculate_expired_date(current_date)
+    date = current_date.strftime('%d/%m/%Y  %H:%M')
+
+    return render_template('index.html', date=date, expired_date=expired_date)
+
+
+@app.route('/')
+def home():
+    oscar_environment_variable = f"{os.environ['OSCAR_DATE']}"
+    oscar_date = datetime.strptime(oscar_environment_variable, "%Y/%m/%d")
+    current_date = datetime.now()
+    difference_day = dates.calculate_difference_day(current_date, oscar_date)
+    year = oscar_date.year
+
+    return render_template('home.html', year=year, difference_day=difference_day)
