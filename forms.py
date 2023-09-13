9d831454ff5b452ffae1d775e00b14cd8b28d851
lais-cardoso <lalaicardoso27@hotmail.com>
2023-09-13 08:25:18 -0300

    fix: reads and prints data on the page

--- forms.py
+++ forms.py
@@ -1,2 +1,5 @@
 def register_form(name: str, email: str, age: int):
     return print(name, email, age)
+
+def profile_form(name: str, email: str, age: int):
+    return print(name, email, age)
--- routes.py
+++ routes.py
@@ -7,18 +7,18 @@ import os
 #local
 import lib.dates as dates
 
-from forms import register_form
+from forms import profile_form, register_form
 
 app = Flask(__name__)
 
 @app.route('/register', methods=["GET", "POST"])
-def login():
+def register():
     if request.method == "POST":
         name = request.form["name"]
         email = request.form["email"]
         age = request.form["age"]
         register_form(name, email, age)
-    return render_template('register.html')
+    return render_template('register.html', name = '', email='', age='')
 
 @app.route('/about')
 def index():
@@ -38,3 +38,11 @@ def home():
     year = oscar_date.year
 
     return render_template('home.html', year=year, difference_day=difference_day)
+
+@app.route('/profile', methods=["GET", "POST"])
+def profile():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    age = request.form.get('age')
+    return render_template('profile.html', name=name, email=email, age=age)
+
